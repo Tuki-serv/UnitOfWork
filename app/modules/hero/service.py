4fe75d1f95a05b810fe8194a2b579da643b24ea7
Tuki-serv <lucasghernandez12@gmail.com>
2026-05-15 14:26:07 -0300

    se pudo el service generico

--- app/modules/hero/service.py
+++ app/modules/hero/service.py
@@ -1,8 +1,9 @@
-from app.core.service import base_service as BaseService
+from app.core.service import base_service
 from app.modules.hero.models import Hero
 from app.modules.hero.schemas import HeroCreate, HeroUpdate
 from app.modules.hero.unit_of_work import HeroUnitOfWork
+from sqlmodel import Session
 
-class HeroService(BaseService[Hero, HeroCreate, HeroUpdate, HeroUnitOfWork]):
-    def __init__(self, uow: HeroUnitOfWork):
-        super().__init__(uow, "heroes", Hero)
+class HeroService(base_service[Hero, HeroCreate, HeroUpdate, HeroUnitOfWork]):
+    def __init__(self, session: Session):
+        super().__init__(session=session,uow_class=HeroUnitOfWork,repo_name="heroes", model_class=Hero)
